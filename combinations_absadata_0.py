f61fb24bbe50e85e36199dc063d63bbabff34a3e
Felix Hamborg <felix@hamborg.eu>
2019-11-21 17:54:19 +0100

    add narrow search

--- combinations_absadata_0.py
+++ combinations_absadata_0.py
@@ -1,9 +1,9 @@
 combinations_absadata_0 = {
     'model_name': [
         # SPC
-        'spc_bert',  # 'spc_distilbert', 'spc_roberta',
+        # 'spc_bert',  # 'spc_distilbert', 'spc_roberta',
         # AEN
-        'aen_bert',  # 'aen_distilbert', 'aen_roberta', 'aen_glove',  # 'aen_distilroberta'
+        #'aen_bert',  # 'aen_distilbert', 'aen_roberta', 'aen_glove',  # 'aen_distilroberta'
         # LCF
         'lcf_bert',
     ],
@@ -16,7 +16,7 @@ combinations_absadata_0 = {
     'devmode': ['False'],
     'num_epoch': ['3', '4'],
     'lsr': ['True', 'False'],
-    'use_tp_placeholders': ['False'],
+    'use_tp_placeholders': ['False', 'True'],
     'spc_lm_representation_distilbert': [  # 'sum_last', 'sum_last_four', 'sum_last_two', 'sum_all',
         'mean_last'],  # , 'mean_last_four', 'mean_last_two', 'mean_all'],
     'spc_lm_representation': ['pooler_output'],
@@ -31,7 +31,8 @@ combinations_absadata_0 = {
     'eval_only_after_last_epoch': ['True'],
     'local_context_focus': ['cdm', 'cdw'],  # ['cdw', 'cdm']
     'SRD': ['3'],
-    'pretrained_model_name': ['bert_news_ccnc_10mio_3ep', 'bert_news_ccnc_10mio_2ep', 'bert_news_ccnc_10mio_1ep',
-                              'default', 'bert_news_ccnc_10mio_3ep', 'laptops_and_restaurants_2mio_ep15',
-                              'laptops_1mio_ep30', 'restaurants_10mio_ep3'],
+    'pretrained_model_name': ['bert_news_ccnc_10mio_3ep'],
+    # 'pretrained_model_name': ['bert_news_ccnc_10mio_3ep', 'bert_news_ccnc_10mio_2ep', 'bert_news_ccnc_10mio_1ep',
+    #                          'default', 'bert_news_ccnc_10mio_3ep', 'laptops_and_restaurants_2mio_ep15',
+    #                          'laptops_1mio_ep30', 'restaurants_10mio_ep3'],
 }
