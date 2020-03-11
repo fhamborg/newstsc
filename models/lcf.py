2456fce1e3edf48202310e73ce3ef3d707399bb4
Felix Hamborg <felix@hamborg.eu>
2020-03-11 11:56:09 +0100

    reformat using black code

--- models/lcf.py
+++ models/lcf.py
@@ -31,8 +31,10 @@ class SelfAttention(nn.Module):
         self.tanh = torch.nn.Tanh()
 
     def forward(self, inputs):
-        zero_tensor = torch.tensor(np.zeros((inputs.size(0), 1, 1, self.opt.max_seq_len),
-                                            dtype=np.float32), dtype=torch.float32).to(self.opt.device)
+        zero_tensor = torch.tensor(
+            np.zeros((inputs.size(0), 1, 1, self.opt.max_seq_len), dtype=np.float32),
+            dtype=torch.float32,
+        ).to(self.opt.device)
         SA_out = self.SA(inputs, zero_tensor)
         return self.tanh(SA_out[0])
 
@@ -46,7 +48,9 @@ class LCF_BERT(nn.Module):
         self.is_global_configuration = is_global_configuration
 
         # self.bert_local = copy.deepcopy(bert)  # Uncomment the line to use dual Bert
-        self.bert_local = bert  # Default to use single Bert and reduce memory requirements
+        self.bert_local = (
+            bert  # Default to use single Bert and reduce memory requirements
+        )
         self.dropout = nn.Dropout(self.opt.dropout)
         self.bert_SA = SelfAttention(bert.config, self.opt)
         self.linear_double = nn.Linear(self.opt.bert_dim * 2, self.opt.bert_dim)
@@ -55,9 +59,13 @@ class LCF_BERT(nn.Module):
 
         if self.opt.use_global_context:
             self.gc_bert = bert
-            self.linear_gc_merger = nn.Linear(self.opt.bert_dim * self.opt.global_context_seqs_per_doc,
-                                              self.opt.bert_dim)
-            self.linear_lcf_and_gc_merger = nn.Linear(self.opt.bert_dim * 2, self.opt.bert_dim)
+            self.linear_gc_merger = nn.Linear(
+                self.opt.bert_dim * self.opt.global_context_seqs_per_doc,
+                self.opt.bert_dim,
+            )
+            self.linear_lcf_and_gc_merger = nn.Linear(
+                self.opt.bert_dim * 2, self.opt.bert_dim
+            )
 
         if not self.is_global_configuration:
             self.dense = nn.Linear(self.opt.bert_dim, self.opt.polarities_dim)
@@ -66,8 +74,10 @@ class LCF_BERT(nn.Module):
         texts = text_local_indices.cpu().numpy()
         asps = aspect_indices.cpu().numpy()
         mask_len = self.opt.SRD
-        masked_text_raw_indices = np.ones((text_local_indices.size(0), self.opt.max_seq_len, self.opt.bert_dim),
-                                          dtype=np.float32)
+        masked_text_raw_indices = np.ones(
+            (text_local_indices.size(0), self.opt.max_seq_len, self.opt.bert_dim),
+            dtype=np.float32,
+        )
         for text_i, asp_i in zip(range(len(texts)), range(len(asps))):
             asp_len = np.count_nonzero(asps[asp_i]) - 2
             try:
@@ -79,17 +89,23 @@ class LCF_BERT(nn.Module):
             else:
                 mask_begin = 0
             for i in range(mask_begin):
-                masked_text_raw_indices[text_i][i] = np.zeros((self.opt.bert_dim), dtype=np.float)
+                masked_text_raw_indices[text_i][i] = np.zeros(
+                    (self.opt.bert_dim), dtype=np.float
+                )
             for j in range(asp_begin + asp_len + mask_len, self.opt.max_seq_len):
-                masked_text_raw_indices[text_i][j] = np.zeros((self.opt.bert_dim), dtype=np.float)
+                masked_text_raw_indices[text_i][j] = np.zeros(
+                    (self.opt.bert_dim), dtype=np.float
+                )
         masked_text_raw_indices = torch.from_numpy(masked_text_raw_indices)
         return masked_text_raw_indices.to(self.opt.device)
 
     def feature_dynamic_weighted(self, text_local_indices, aspect_indices):
         texts = text_local_indices.cpu().numpy()
         asps = aspect_indices.cpu().numpy()
-        masked_text_raw_indices = np.ones((text_local_indices.size(0), self.opt.max_seq_len, self.opt.bert_dim),
-                                          dtype=np.float32)
+        masked_text_raw_indices = np.ones(
+            (text_local_indices.size(0), self.opt.max_seq_len, self.opt.bert_dim),
+            dtype=np.float32,
+        )
         for text_i, asp_i in zip(range(len(texts)), range(len(asps))):
             asp_len = np.count_nonzero(asps[asp_i]) - 2
             try:
@@ -100,18 +116,24 @@ class LCF_BERT(nn.Module):
             distances = np.zeros(np.count_nonzero(texts[text_i]), dtype=np.float32)
             for i in range(1, np.count_nonzero(texts[text_i]) - 1):
                 if abs(i - asp_avg_index) + asp_len / 2 > self.opt.SRD:
-                    distances[i] = 1 - (abs(i - asp_avg_index) + asp_len / 2
-                                        - self.opt.SRD) / np.count_nonzero(texts[text_i])
+                    distances[i] = 1 - (
+                        abs(i - asp_avg_index) + asp_len / 2 - self.opt.SRD
+                    ) / np.count_nonzero(texts[text_i])
                 else:
                     distances[i] = 1
             for i in range(len(distances)):
-                masked_text_raw_indices[text_i][i] = masked_text_raw_indices[text_i][i] * distances[i]
+                masked_text_raw_indices[text_i][i] = (
+                    masked_text_raw_indices[text_i][i] * distances[i]
+                )
         masked_text_raw_indices = torch.from_numpy(masked_text_raw_indices)
         return masked_text_raw_indices.to(self.opt.device)
 
     def _get_inputs_for_global_context_segment(self, inputs, segment_index, offset):
-        assert segment_index < self.opt.global_context_seqs_per_doc, "segment_index({}) >= max_num_components({})".format(
-            segment_index, self.opt.global_context_seqs_per_doc)
+        assert (
+            segment_index < self.opt.global_context_seqs_per_doc
+        ), "segment_index({}) >= max_num_components({})".format(
+            segment_index, self.opt.global_context_seqs_per_doc
+        )
 
         in0 = inputs[offset + segment_index * 3]
         in1 = inputs[offset + segment_index * 3 + 1]
@@ -120,8 +142,12 @@ class LCF_BERT(nn.Module):
         return in0, in1, in2
 
     def forward(self, inputs):
-        text_bert_indices, bert_segments_ids, text_local_indices, aspect_indices = inputs[0], inputs[1], inputs[2], \
-                                                                                   inputs[3]
+        text_bert_indices, bert_segments_ids, text_local_indices, aspect_indices = (
+            inputs[0],
+            inputs[1],
+            inputs[2],
+            inputs[3],
+        )
         batch_size = text_bert_indices.shape[0]
 
         bert_spc_out, _, _ = self.bert_spc(text_bert_indices, bert_segments_ids)
@@ -130,12 +156,16 @@ class LCF_BERT(nn.Module):
         bert_local_out, _, _ = self.bert_local(text_local_indices)
         bert_local_out = self.dropout(bert_local_out)
 
-        if self.opt.local_context_focus == 'cdm':
-            masked_local_text_vec = self.feature_dynamic_mask(text_local_indices, aspect_indices)
+        if self.opt.local_context_focus == "cdm":
+            masked_local_text_vec = self.feature_dynamic_mask(
+                text_local_indices, aspect_indices
+            )
             bert_local_out = torch.mul(bert_local_out, masked_local_text_vec)
 
-        elif self.opt.local_context_focus == 'cdw':
-            weighted_text_local_features = self.feature_dynamic_weighted(text_local_indices, aspect_indices)
+        elif self.opt.local_context_focus == "cdw":
+            weighted_text_local_features = self.feature_dynamic_weighted(
+                text_local_indices, aspect_indices
+            )
             bert_local_out = torch.mul(bert_local_out, weighted_text_local_features)
 
         out_cat = torch.cat((bert_local_out, bert_spc_out), dim=-1)
@@ -147,7 +177,9 @@ class LCF_BERT(nn.Module):
             lst_gc_segments = []
 
             for i in range(self.opt.global_context_seqs_per_doc):
-                gci, gct, gca = self._get_inputs_for_global_context_segment(inputs, i, 4)
+                gci, gct, gca = self._get_inputs_for_global_context_segment(
+                    inputs, i, 4
+                )
                 _, gc_bert_pooler_output, _ = self.gc_bert(gci, gct, gca)
                 gc_bert_pooler_output = self.dropout(gc_bert_pooler_output)
                 lst_gc_segments.append(gc_bert_pooler_output)
@@ -155,8 +187,12 @@ class LCF_BERT(nn.Module):
             # merge with main output
             gc_segments = torch.cat(lst_gc_segments, dim=-1)
             merged_gc_segments = self.linear_gc_merger(gc_segments)
-            merged_pooled_out_and_global_context = torch.cat((pooled_out, merged_gc_segments), dim=-1)
-            pooled_out = self.linear_lcf_and_gc_merger(merged_pooled_out_and_global_context)
+            merged_pooled_out_and_global_context = torch.cat(
+                (pooled_out, merged_gc_segments), dim=-1
+            )
+            pooled_out = self.linear_lcf_and_gc_merger(
+                merged_pooled_out_and_global_context
+            )
 
         # if we use LCF as is, i.e., not in the context of globalsenti, then apply the original last dense layer to get
         # 3 classes for each document
